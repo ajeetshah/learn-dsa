ba52bef6e7da2866f614fd210042a7bef5a79091
Ajeet <ajeets.shah.87@gmail.com>
2025-02-18 14:49:13 +0530

    Add java file

---
+++ leetcode-75/MergeStringsAlternately.java
@@ -0,0 +1,11 @@
+class MergeStringsAlternatively {
+
+    public static String mergeAlternately() {
+        return "";
+	}
+
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
