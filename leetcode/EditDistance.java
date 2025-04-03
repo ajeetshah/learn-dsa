760730f5baa12f5087d5059bfe2ee3b49015b908
Ajeet <ajeet-public-repo@example.com>
2025-04-03 18:28:24 +0530

    Add question link

--- leetcode/EditDistance.java
+++ leetcode/EditDistance.java
@@ -1,3 +1,16 @@
+/*
+https://leetcode.com/problems/edit-distance/description/
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+*/
+
 import java.util.HashMap;
 
 class EditDistance {
